fac10b3740682776fd5ef557b28440de0d5a29c3
DarkcTime <vlad_chekmarev_01@mail.ru>
2022-07-14 16:24:08 +0300

    front for auth form

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
 class auth_app_form(forms.Form):
-    Login = forms.CharField(max_length=100, label='Логин или email')
+    Login = forms.CharField(max_length=100, label='Логин')
     Password = forms.CharField(max_length=100, label='Пароль')
-    RememberMe = forms.BooleanField(label='Запомнить меня')
